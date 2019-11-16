309065f5e264d4dd0e3afd8fe754520180d1b746
Shoutokobayashi <deepstrap353@gmail.com>
2019-11-16 09:44:56 +0900

    poster Ehime

--- app/File.php
+++ app/File.php
@@ -2,24 +2,8 @@
 
 namespace App;
 
-// use App\Description;
-// use App\Folder;
-// use Illuminate\Database\Eloquent\Model;
-
 class File /*extends Model*/
 {
-
-    // protected $fillable = ["name", "text", "index", "parent_folder_id", "lesson_id"];
-
-    // public function parentFolder()
-    // {
-    //     return $this->belongsTo(Folder::class, "parent_folder_id");
-    // }
-
-    // public function descriptions()
-    // {
-    //     return $this->hasMany(Description::class);
-    // }
 
     public $path = "";
     public $text = "";
--- app/Question.php
+++ app/Question.php
@@ -27,21 +27,3 @@ class Question
     }
 }
 
-// use App\Description;
-// use App\InputButton;
-// use Illuminate\Database\Eloquent\Model;
-
-// class Question extends Model
-// {
-//     protected $fillable = ["index", "description_id"];
-
-//     public function description()
-//     {
-//         return $this->belongsTo(Description::class);
-//     }
-
-//     public function inputButtons()
-//     {
-//         return $this->hasMany(InputButton::class);
-//     }
-// }
